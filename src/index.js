93ae723ab7b7ec09b7267a76ab3ee9a81da2e3a4
Geraud Oliveira <geraud.pereira@hotmail.com>
2024-01-09 18:34:27 -0300

    Aula II

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import './style.scss'
+
 function tittleComponent(){
     const elemH1 = document.createElement('h1')
     elemH1.innerHTML = 'Hello World'
+    elemH1.classList.add('title')
     return elemH1
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,43 @@
 const path = require('path')
+const HtmlWebpack = require('html-webpack-plugin')
+const MiniCssExtract = require ('mini-css-extract-plugin')
 
 module.exports = {
     entry:'./src/index.js',
     output: {
         filename:'main.js',
         path: path.resolve(__dirname, 'dist')
-    }
+    },
+
+    module:{
+        rules: [
+            {
+                test: /\.(sa|c|sc)ss$/,
+                use: [
+                    MiniCssExtract.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.css$/i,
+                    use: [
+                        'style.loader',
+                        'css-loader'
+                    ]
+            }    
+                
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpack ({
+            filename: 'index.html',
+            template: './src/index.html'
+        }),
+        new MiniCssExtract({
+            filename: 'style.css'
+        })
+    ]
+
 }
