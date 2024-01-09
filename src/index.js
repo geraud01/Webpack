f532ade1d6ffadaf724d2f55879931dee2dd79bb
Geraud Oliveira <geraud.pereira@hotmail.com>
2024-01-09 19:56:03 -0300

    Exercicio

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import './style.scss'
+import img from './matrix.png'
 
-function tittleComponent(){
+function rootStyle(){
+    const elemRoot = document.getElementById('root')
+    elemRoot.classList.add('container')
+}
+
+function titleComponent(){
     const elemH1 = document.createElement('h1')
-    elemH1.innerHTML = 'Hello World'
+    elemH1.innerHTML = 'Hello world'
     elemH1.classList.add('title')
     return elemH1
 }
 
-document.body.appendChild(tittleComponent)
+function imageComponent(){
+    const elemImg = new Image()
+    elemImg.src = img
+    return elemImg
+}
+
+rootStyle()
+document.getElementById('root').appendChild(titleComponent())
+document.getElementById('root').appendChild(imageComponent())
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
 const path = require('path')
 const HtmlWebpack = require('html-webpack-plugin')
-const MiniCssExtract = require ('mini-css-extract-plugin')
+const MiniCssExtract = require('mini-css-extract-plugin')
 
 module.exports = {
-    entry:'./src/index.js',
+
+    entry: './src/index.js',
     output: {
-        filename:'main.js',
+        filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
-
-    module:{
+    module: {
         rules: [
             {
-                test: /\.(sa|c|sc)ss$/,
+                test: /\.(sa|c|sc)ss$/i, 
                 use: [
                     MiniCssExtract.loader,
                     'css-loader',
@@ -21,17 +21,34 @@ module.exports = {
             },
             {
                 test: /\.css$/i,
-                    use: [
-                        'style.loader',
-                        'css-loader'
-                    ]
-            }    
-                
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.js$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(jpeg|jpg|png|svg|gif)$/i,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]'
+                    }
+                }
+            }
         ]
     },
-
     plugins: [
-        new HtmlWebpack ({
+        new HtmlWebpack({
             filename: 'index.html',
             template: './src/index.html'
         }),
